f9390b15ce23fbf7cca18725a1450dca30f9d42b
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2021-01-05 22:49:19 +0000

    add support for complex settings and class file

--- Program.cs
+++ Program.cs
@@ -6,13 +6,27 @@ namespace SampleConfig
 {
     class Program
     {
+
+        public class WeatherClientConfig
+        {
+            public bool IsEnabled { get; set; }
+            public string WeatherAPIUrl { get; set; }
+            public string Timeout { get; set; }
+
+            public override string ToString()
+            {                
+                return "Is Enabled: " + IsEnabled + ", WeatherAPIUrl: " + WeatherAPIUrl + ", Timeout: " + Timeout;                
+            }
+
+        } // end class weather
+
         public static IConfiguration Configuration { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-            
+
             if (String.IsNullOrEmpty(env))
             {   //the env is null so run as production
                 env = "Production";
@@ -31,11 +45,20 @@ namespace SampleConfig
             string mySecretSetting = Configuration["SqlPassword"];
             string myDynamicAppSetting = Configuration["EnvironmentValue"];
 
+            var testValue = Configuration["EmailSettings:Email"];
+            var section = Configuration.GetSection(nameof(WeatherClientConfig));
+            var weatherClientConfig = section.Get<WeatherClientConfig>();
+
             Console.WriteLine($"My environment name is: {env}");
             Console.WriteLine($"Setting from AppSettings: {myAppSetting}");
             Console.WriteLine($"Setting from AppDevSettings: {myDevAppSetting}");
             Console.WriteLine($"Setting from Secrets: {mySecretSetting}");
             Console.WriteLine($"Setting from AppSettings or AppDevSettings : {myDynamicAppSetting}");
+
+            Console.WriteLine($"Setting from complex AppSettings via direct reference: {testValue}");
+            Console.WriteLine($"Setting from complex AppSettings via C# class: {weatherClientConfig}");
+
+
 
         }
     }
